09e77d8f1a16fe304a8e6a11038f607071669173
Technoranti <ranting.tech@gmail.com>
2025-05-19 17:33:00 +0800

    gpt-image-1 update

--- sandbox.py
+++ sandbox.py
@@ -1,16 +1,25 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+import base64
 
 load_dotenv('.env')
 openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
 response = client.images.generate(
-    model="dall-e-2",
+    model="gpt-image-1",
     prompt="A cute grey cat wearing round orange glasses",
     n=1,
-    size="1024x1024", #options are 256x256, 512x512, 1024x1024
+    quality="medium", #options are low, medium, high
+    size="1024x1024", #options are 1024x1024, 1024x1536, 1536x1024
     )
+
+image_base64 = response.data[0].b64_json
+image_bytes = base64.b64decode(image_base64)
+
+# Save the image to a file
+with open("Image Generations/Test1.png", "wb") as f:
+    f.write(image_bytes)
 
 print(response) #click on the url that appear in the terminal output to see the image. Next update will automaticall open the image in browser.
