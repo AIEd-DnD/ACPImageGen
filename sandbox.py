3d6b3e0462295bb0fd0cddef58603281619dabd1
Technoranti <ranting.tech@gmail.com>
2025-05-22 17:26:49 +0800

    minor clean up for useability

--- sandbox.py
+++ sandbox.py
@@ -22,19 +22,6 @@ for i in range(number_of_images):
     with open(f"Image Generations/CuteCat_low_illustrated_test_{i+1}.png", "wb") as f:
         image_bytes = base64.b64decode(image_base64)
         f.write(image_bytes)
-#image_base64_1 = response.data[0].b64_json
-#image_base64_2 = response.data[1].b64_json
-#image_base64_3 = response.data[2].b64_json
-#image_bytes_1 = base64.b64decode(image_base64_1)
-#image_bytes_2 = base64.b64decode(image_base64_2)
-#image_bytes_3 = base64.b64decode(image_base64_3)
+    print(f"Image {i+1} saved as CuteCat_low_illustrated_test_{i+1}.png")
 
-# Save the image to a file
-#with open("Image Generations/TestChinese_low_photoreal_testn1.png", "wb") as f:
-    #f.write(image_bytes_1)
-#with open("Image Generations/TestChinese_low_photoreal_testn2.png", "wb") as f:
-    #f.write(image_bytes_2)
-#with open("Image Generations/TestChinese_low_photoreal_testn3.png", "wb") as f:
-    #f.write(image_bytes_3)
-
-print(response) #click on the url that appear in the terminal output to see the image. Next update will automaticall open the image in browser.
+#print(response) #click on the url that appear in the terminal output to see the image. Next update will automaticall open the image in browser.
